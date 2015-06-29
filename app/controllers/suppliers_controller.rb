3160b22d5558d4df3533aebdd4e4a5bfa2b3bf39
Manuel Schnitzer <mschnitzer@suse.de>
2015-06-29 13:37:02 +0200

    introduce Suppliers

---
+++ app/controllers/suppliers_controller.rb
@@ -0,0 +1,47 @@
+class SuppliersController < ApplicationController
+
+  def index
+    @suppliers = Supplier.all
+  end
+
+  def new
+    @supplier = Supplier.new
+  end
+
+  def edit
+    @supplier = Supplier.find(params[:id])
+  end
+
+  def create
+    @supplier = Supplier.new(supplier_params)
+
+    if @supplier.save
+      redirect_to action: 'index'
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @supplier = Supplier.find(params[:id])
+
+    if @supplier.update(supplier_params)
+      redirect_to action: 'index'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @supplier = Supplier.find(params[:id])
+    @supplier.destroy
+
+    redirect_to suppliers_path
+  end
+
+  private
+  def supplier_params
+    params.require(:supplier).permit(:title)
+  end
+
+end
