81ccc48a31c12c91eb72af8411b65d35c1ad7bcd
Vanessa Wallfahrer <vwallfahrer@suse.de>
2015-06-30 10:22:30 +0200

    add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,14 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   cities = City.create([{ title: 'Chicago' }, { title: 'Copenhagen' }])
+#   Mayor.create(title: 'Emanuel', city: cities.first)
+
+suppliers = Supplier.create([{title: 'HP'}, {title: 'Lenovo'}, {title: 'DELL'}])
+rooms = Room.create([{title: 'R204'}, {title: 'R354'}, {title: 'R9384'}])
+category = Category.create([{title: 'PC'}, {title: 'Beamer'}, {title: 'Beamer'}])
+
+Component.create(title:"Cooler Computer", supplier: suppliers.first, room: rooms.first, category: category.first)
+Component.create(title:"Druckender Drucker", supplier: suppliers[1..3].sample, room: rooms[1..3].sample, category: category[1..3].sample)
+Component.create(title:"Laser Bean", supplier: suppliers[1..3].sample, room: rooms[1..3].sample, category: category[1..3].sample)
+Component.create(title:"Klappernde Tastatur", supplier: suppliers[1..3].sample, room: rooms[1..3].sample, category: category[1..3].sample)
