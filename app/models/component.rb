dfe5da6331da7f8521ead4bf931e91d6e3c9e28c
Vanessa Wallfahrer <vwallfahrer@suse.de>
2015-06-30 09:45:04 +0200

    add supplier_id to Component

--- app/models/component.rb
+++ app/models/component.rb
@@ -1,9 +1,11 @@
 class Component < ActiveRecord::Base
   belongs_to :category
   belongs_to :room
+  belongs_to :supplier
 
   validates :title, presence: true,
             length: { minimum: 2 }
   validates :category_id, presence: true
   validates :room_id, presence: true
+  validates :supplier_id, presence: true
 end
---
+++ db/migrate/20150630073454_add_supplier_id_to_component.rb
@@ -0,0 +1,5 @@
+class AddSupplierIdToComponent < ActiveRecord::Migration
+  def change
+    add_column :components, :supplier_id, :integer
+  end
+end
