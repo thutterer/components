c18b51e15290d20f766958886a2be94b19bbb192
Thomas Hutterer <thutterer@suse.de>
2015-07-01 19:43:31 +0200

    validate presence of quantity

--- app/models/component.rb
+++ app/models/component.rb
@@ -4,6 +4,7 @@ class Component < ActiveRecord::Base
   belongs_to :supplier
 
   validates :title, presence: {message: :bad_blank}
+  validates :quantity, presence: {message: :bad_blank}
   validates_numericality_of :quantity, only_integer: true, message: :not_integer
   validates_numericality_of :quantity, greater_than_or_equal_to: 0,
                             message: :negative_number
