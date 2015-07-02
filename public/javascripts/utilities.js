df718a26259b46b587b333a92d22d8b79c3cae05
Manuel Schnitzer <mschnitzer@suse.de>
2015-07-02 15:34:10 +0200

    add jQuery slide to search container
    (cherry picked from commit 974030defc189289e517daf3c16e2fd98ed25d37)

--- public/javascripts/utilities.js
+++ public/javascripts/utilities.js
@@ -5,13 +5,17 @@ $(function () {
 function display_search() {
     index = document.getElementById("search-container");
     if (index.style.display == "none") {
-        index.style.display = "block";
         img = document.getElementById("search-display-icon");
         img.setAttribute("src", "images/minusM.png");
+
+        $( "#search-container" ).slideDown( "500", function() {
+        });
     }
     else {
-        index.style.display = "none";
         img = document.getElementById("search-display-icon");
         img.setAttribute("src", "images/plusM.png");
+
+        $( "#search-container" ).slideUp( "500", function() {
+        });
     }
 }
