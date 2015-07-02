1dee926d830ba55be81f9fe46c5637f05ddcb44c
Manuel Schnitzer <mschnitzer@suse.de>
2015-07-02 15:08:59 +0200

    add javascript to search function

--- public/javascripts/utilities.js
+++ public/javascripts/utilities.js
@@ -1,3 +1,17 @@
 $(function () {
     $("[rel='tooltip']").tooltip();
 });
+
+function display_search() {
+    index = document.getElementById("search-container");
+    if (index.style.display == "none") {
+        index.style.display = "block";
+        img = document.getElementById("search-display-icon");
+        img.setAttribute("src", "images/minusM.png");
+    }
+    else {
+        index.style.display = "none";
+        img = document.getElementById("search-display-icon");
+        img.setAttribute("src", "images/plusM.png");
+    }
+}
